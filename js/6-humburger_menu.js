fdb93c59056ba199e85a9efe4107e94feb465265
Mehdi <zamanianmehdi23desktop@gmail.com>
2024-12-28 22:15:59 +0330

    menubar added

---
+++ js/6-humburger_menu.js
@@ -0,0 +1,29 @@
+
+
+const humburger_menu = document.querySelector('#humburger_menu')
+
+
+
+humburger_menu.addEventListener('click', () => {
+  counter++;
+  if(counter==1){
+    const menu = document.getElementById('mymenu').style.bottom='0px';
+    humburger_menu.src="images/close.png";
+
+    const menutopdiv=document.getElementById("menutopdiv")
+    menutopdiv.style.backgroundColor="#111827"
+    
+    
+  }
+  if(counter>1){
+    const menu = document.getElementById('mymenu').style.bottom='-49%';
+    humburger_menu.src="images/bar.png";
+
+        const menutopdiv=document.getElementById("menutopdiv")
+    menutopdiv.style.backgroundColor="white"
+
+    counter=0
+
+  }
+  
+})

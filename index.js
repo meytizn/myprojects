72c6c323efc118ef3a29ed32ea4d5c851ad6a9aa
Mehdi <zamanianmehdi23desktop@gmail.com>
2024-12-31 16:47:59 +0330

    themchanger bug fixed

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-const navabar = document.getElementById("navbar")
-const btnthem = document.getElementById("btnthem")
+// const navabar = document.getElementById("navbar")
+// const btnthem = document.getElementById("btnthem")
 
 
-let color_list = ['red', 'blue', 'green', 'yellow']
-let counter = 0
-console.log(counter)
-console.log(color_list)
-console.log(color_list.length)
+// let color_list = ['red', 'blue', 'green', 'yellow']
+// let counter = 0
+// console.log(counter)
+// console.log(color_list)
+// console.log(color_list.length)
 
-btnthem.addEventListener('click', function navcolor() {
-  counter += 1;
-  console.log(counter)
-  navabar.style.backgroundColor = color_list[counter]
-  if (counter === color_list.length - 1) {
-    // alert('no color else');
-    counter = -1
-  }
+// btnthem.addEventListener('click', function navcolor() {
+//   counter += 1;
+//   console.log(counter)
+//   navabar.style.backgroundColor = color_list[counter]
+//   if (counter === color_list.length - 1) {
+//     // alert('no color else');
+//     counter = -1
+//   }
 
-})
+// })
 
 
 
--- js/1-themChanger.js
+++ js/1-themChanger.js
@@ -1,58 +1,22 @@
-// const navabar = document.getElementById("navbar")
-// const btnthem = document.getElementById("btnthem")
 
 
-// let color_list = ['red', 'blue', 'green', 'yellow']
-// let counter = 0
-// console.log(counter)
-// console.log(color_list)
-// console.log(color_list.length)
+let counter = 0
+const them_list = ['gray', 'black', 'red', 'orange', 'green']
 
-// btnthem.addEventListener('click', function navcolor() {
-//   counter += 1;
-//   console.log(counter)
-//   navabar.style.backgroundColor = color_list[counter]
-//   if (counter === color_list.length - 1) {
-//     // alert('no color else');
-//     counter = -1
-//   }
+function themchanger() {
 
-// })
+  if (counter >= them_list.length - 1) {
 
+    counter = 0
+    const body = document.getElementById('body').style.backgroundColor = them_list[counter]
 
-const btn1 = document.querySelector('#themchanger')
-const body = document.querySelector('#body')
-const navbar = document.querySelector('#navbar')
-
-color_list = ['blue', 'orange', 'pink',] 
-console.log(color_list)
-let counter = 0           
-
-
-btn1.addEventListener('click', (event) => {
-  event.preventDefault();
-  
-  counter += 1;
-  console.log(counter);
-  body.style.backgroundColor = color_list[counter];
-  navbar.style.backgroundColor = color_list[counter]; //0[1] , 1[2]
-  btn1.textContent = `# ${color_list[counter]} them  successfully added  . `;
-
-
-  const defbtn = document.createElement('button');
-  defbtn.textContent = '    click to reset color    ';
-  defbtn.style.color = 'red';
-  btn1.appendChild(defbtn);
-
-    if(counter>=color_list.length-1){counter=-1};
-
-
-  defbtn.addEventListener('click', () => {
-    location.reload();
-  })
-
-})
-
-
-
-
+  } else {
+    counter++;
+    const body = document.getElementById('body').style.backgroundColor = them_list[counter]
+    const navbar_first = document.getElementById('navbar_first').style.backgroundColor = them_list[counter]
+    const navbar_second = document.getElementById('navbar_second').style.backgroundColor = them_list[counter]
+    const menutopdiv = document.getElementById('menutopdiv').style.backgroundColor = them_list[1]
+    const metacolor = document.getElementById('metacolor').setAttribute('content', them_list[1])
+    console.log(counter)
+  }
+}

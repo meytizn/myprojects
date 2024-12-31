b85efa6450d4190dcbaf86ed31e9420d81fec4a4
Mehdi <zamanianmehdi23desktop@gmail.com>
2024-12-31 16:56:31 +0330

    Update 1-themChanger.js

--- js/1-themChanger.js
+++ js/1-themChanger.js
@@ -8,7 +8,12 @@ function themchanger() {
   if (counter >= them_list.length - 1) {
 
     counter = 0
+
     const body = document.getElementById('body').style.backgroundColor = them_list[counter]
+    const navbar_first = document.getElementById('navbar_first').style.backgroundColor = them_list[counter]
+    const navbar_second = document.getElementById('navbar_second').style.backgroundColor = them_list[counter]
+    const menutopdiv = document.getElementById('menutopdiv').style.backgroundColor = them_list[1]
+    const metacolor = document.getElementById('metacolor').setAttribute('content', them_list[counter])
 
   } else {
     counter++;
